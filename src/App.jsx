e5774dec840239eb9aab895056241ddd5a53f256
juaniB12 <bjuan.ignacio11@gmail.com>
2024-05-14 19:55:59 -0300

    css

--- src/App.jsx
+++ src/App.jsx
@@ -1,11 +1,11 @@
-import { Navbar } from "./components/Navbar"
 import './css/style.css' 
+import { Home } from "./pages/Home"
 
 function App() {
 
   return (
     <>
-      <Navbar />
+      <Home />
     </>
   )
 }
--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -1,5 +1,5 @@
 import '../css/Navbar.css'
-import { User, Search, Favorite, Cart, Menu } from '../icons/Icons'
+import { User, Search, Favorite, Cart, Menu, Close } from '../icons/Icons'
 
 export const Navbar = () => {
     return (
@@ -41,53 +41,48 @@ export const Navbar = () => {
                     </div>
                 </ul>
 
-                <div class="navbar-responsive">
-                        <label for="menu">
-                            <Menu />
-                        </label>
-                        <input type="checkbox" name="menu-responsive" id="menu" />
-                        <article class="popup">
-                            <label class="background" for="menu"></label>
-                            <ul>
-                                <li class="menu-close">
-                                    <em>Men&uacute;</em>
-                                    <label for="menu">
-                                        <i class="icon close"></i>
-                                    </label>
-                                </li>
-                                <li>
-                                    <a href="#inicio">
-                                        <i class="icon home"></i>
-                                        <span>Inicio</span>
-                                    </a>
-                                </li>
-                                <li>
-                                    <a href="#sobre-nosotros">
-                                        <i class="icon about-us"></i>
-                                        <span>Sobre nosotros</span>
-                                    </a>
-                                </li>
-                                <li>
-                                    <a href="#servicios">
-                                        <i class="icon service"></i>
-                                        <span>Servicios</span>
-                                    </a>
-                                </li>
-                                <li>
-                                    <a href="#preguntas">
-                                        <i class="icon message"></i>
-                                        <span>Preguntas</span>
-                                    </a>
-                                </li>
-                                <li>
-                                    <a href="#contacto">
-                                        <i class="icon mail"></i>
-                                        <span>Contacto</span>
-                                    </a>
-                                </li>
-                            </ul>
-                        </article>
-                    </div>
+                {/* <div className="navbar-responsive">
+                    <label for="menu">
+                        <Menu />
+                    </label>
+                    <input type="checkbox" name="menu-responsive" id="menu" />
+                    <article className="popup">
+                        <label className="background" for="menu"></label>
+                        <ul>
+                            <li className="menu-close">
+                                <em>Men&uacute;</em>
+                                <label for="menu">
+                                    <Close />
+                                </label>
+                            </li>
+                            <li>
+                                <a href="#">
+                                    <span>Inicio</span>
+                                </a>
+                            </li>
+                            <li>
+                                <a href="#">
+                                    <span>Tienda</span>
+                                </a>
+                            </li>
+                            <li>
+                                <a href="#">
+                                    <span>Nosotros</span>
+                                </a>
+                            </li>
+                            <li>
+                                <a href="#">
+                                    <span>Contacto</span>
+                                </a>
+                            </li>
+                            <li>
+                                <a href="#">
+                                    <span>Contacto</span>
+                                </a>
+                            </li>
+                        </ul>
+                    </article>
+                </div> */}
             </nav>
         </header>
     )
