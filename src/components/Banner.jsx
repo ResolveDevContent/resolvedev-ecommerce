2644a971c5714e18cac338b82c2c6df03e537daf
juaniB12 <bjuan.ignacio11@gmail.com>
2024-05-14 20:00:48 -0300

    css

--- src/components/Banner.jsx
+++ src/components/Banner.jsx
@@ -2,8 +2,26 @@
 
 export const Banner = () => {
     return (
-        <div>
-            
-        </div>
+            <section id="inicio">
+            <ul>
+                <li>
+                    <figure>
+                        <figcaption>
+                            <article>
+                                <div className="home-text">
+                                    <img src="" alt="logo" /> 
+                                    <p>
+                                        Lorem ipsum dolor sit amet, consectetur adipiscing elit
+                                    </p>
+                                </div>
+                                <footer>
+                                    <a href="#servicios">¡Comencemos!</a>
+                                </footer>
+                            </article>
+                        </figcaption>
+                    </figure>
+                </li>
+            </ul>
+        </section>
     )
 }
