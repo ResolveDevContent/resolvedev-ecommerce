64665af405fedbaa85c43a6a6e9770545ba774ec
juaniB12 <bjuan.ignacio11@gmail.com>
2024-05-14 19:13:51 -0300

    add icons

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -1,6 +1,43 @@
+import '../css/Navbar.css'
 
 export const Navbar = () => {
     return (
-        <div>Navbar</div>
+        <header>
+            <ul>
+                <div className='logo'>
+                    <a href="#">Logo</a>
+                </div>
+                <ul>
+                    <li>
+                        <a href="#">Inicio</a>
+                    </li>
+                    <li>
+                        <a href="#">Tienda</a>
+                    </li>
+                    <li>
+                        <a href="#">Nosotros</a>
+                    </li>
+                    <li>
+                        <a href="#">Contacto</a>
+                    </li>
+                </ul>
+                <div className='buttons'>
+                    <ul>
+                        <li>
+                            <a href="#">Login</a>
+                        </li>
+                        <li>
+                            <a href="#">Buscar</a>
+                        </li>
+                        <li>
+                            <a href="#">Favoritos</a>
+                        </li>
+                        <li>
+                            <a href="#">Cart</a>
+                        </li>
+                    </ul>
+                </div>
+            </ul>
+        </header>
     )
 }
