3119728f351132598e79d0d4601d2cc9ae98bac9
juaniB12 <bjuan.ignacio11@gmail.com>
2024-05-14 19:35:40 -0300

    add global css

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -1,44 +1,94 @@
 import '../css/Navbar.css'
-import { User, Search, Favorite, Cart } from '../icons/Icons'
+import { User, Search, Favorite, Cart, Menu } from '../icons/Icons'
 
 export const Navbar = () => {
     return (
-        <header className='navbar'>
-            <ul>
-                <div className='logo'>
-                    <a href="#">Logo</a>
-                </div>
-                <ul>
-                    <li>
-                        <a href="#">Inicio</a>
-                    </li>
-                    <li>
-                        <a href="#">Tienda</a>
-                    </li>
-                    <li>
-                        <a href="#">Nosotros</a>
-                    </li>
-                    <li>
-                        <a href="#">Contacto</a>
-                    </li>
-                </ul>
-                <div className='buttons'>
-                    <ul>
+        <header>
+            <nav className='navbar'>
+                <ul className='navbar-list'>
+                    <div className='logo'>
+                        <a href="#">Logo</a>
+                    </div>
+                    <ul className='navbar-items'>
                         <li>
-                            <User />
+                            <a href="#">Inicio</a>
                         </li>
                         <li>
-                            <Search />
+                            <a href="#">Tienda</a>
                         </li>
                         <li>
-                            <Favorite />
+                            <a href="#">Nosotros</a>
                         </li>
                         <li>
-                            <Cart />
+                            <a href="#">Contacto</a>
                         </li>
                     </ul>
-                </div>
-            </ul>
+                    <div className='buttons'>
+                        <ul>
+                            <li>
+                                <User />
+                            </li>
+                            <li>
+                                <Search />
+                            </li>
+                            <li>
+                                <Favorite />
+                            </li>
+                            <li>
+                                <Cart />
+                            </li>
+                        </ul>
+                    </div>
+                </ul>
+
+                <div class="navbar-responsive">
+                        <label for="menu">
+                            <Menu />
+                        </label>
+                        <input type="checkbox" name="menu-responsive" id="menu" />
+                        <article class="popup">
+                            <label class="background" for="menu"></label>
+                            <ul>
+                                <li class="menu-close">
+                                    <em>Men&uacute;</em>
+                                    <label for="menu">
+                                        <i class="icon close"></i>
+                                    </label>
+                                </li>
+                                <li>
+                                    <a href="#inicio">
+                                        <i class="icon home"></i>
+                                        <span>Inicio</span>
+                                    </a>
+                                </li>
+                                <li>
+                                    <a href="#sobre-nosotros">
+                                        <i class="icon about-us"></i>
+                                        <span>Sobre nosotros</span>
+                                    </a>
+                                </li>
+                                <li>
+                                    <a href="#servicios">
+                                        <i class="icon service"></i>
+                                        <span>Servicios</span>
+                                    </a>
+                                </li>
+                                <li>
+                                    <a href="#preguntas">
+                                        <i class="icon message"></i>
+                                        <span>Preguntas</span>
+                                    </a>
+                                </li>
+                                <li>
+                                    <a href="#contacto">
+                                        <i class="icon mail"></i>
+                                        <span>Contacto</span>
+                                    </a>
+                                </li>
+                            </ul>
+                        </article>
+                    </div>
+            </nav>
         </header>
     )
 }
--- src/icons/Icons.jsx
+++ src/icons/Icons.jsx
@@ -32,3 +32,19 @@ export const Cart = () => {
         </svg>
     )
 }
+
+export const Menu = () => {
+    return (
+        <svg xmlns="http://www.w3.org/2000/svg" width="24" height="24" viewBox="0 0 24 24">
+            <path d="M4 6h16v2H4zm0 5h16v2H4zm0 5h16v2H4z"></path>
+        </svg>
+    )
+}
+
+export const Close = () => {
+    return (
+        <svg xmlns="http://www.w3.org/2000/svg" width="24" height="24" viewBox="0 0 24 24">
+            <path d="m16.192 6.344-4.243 4.242-4.242-4.242-1.414 1.414L10.535 12l-4.242 4.242 1.414 1.414 4.242-4.242 4.243 4.242 1.414-1.414L13.364 12l4.242-4.242z"></path>
+        </svg>
+    )
+}
