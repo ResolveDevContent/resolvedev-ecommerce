4a775ad45c291e95fc366e7fd6d93230561a8354
juaniB12 <bjuan.ignacio11@gmail.com>
2024-05-15 18:25:25 -0300

    changes css

--- src/components/Product.jsx
+++ src/components/Product.jsx
@@ -1,50 +1,83 @@
 import '../css/Product.css'
+import { Plus, Minus, Share } from '../icons/Icons'
+import Img from '../images/img.png'
 
 export const Product = () => {
     return (
-        <section className="product-page">
-            <aside className="product-images">
-                <ul>
+        <>
+            <section className="product-page">
+                <aside className="product-images">
+                    <figure>
+                        <img src={Img} alt="" />
+                    </figure>
+                    <ul>
                     <li>
-                        <img src="#" alt="" />
+                        <figure>
+                            <img src={Img} alt="" />
+                        </figure>
                     </li>
-                </ul>
-            </aside>
-            <article className="product-details">
-                <header>
-                    <strong>Titulo</strong>
-                    <em>Precio descuento</em>
-                    <em>Precio</em>
-                </header>
-                <div className="caracteristicas">
+                    <li>
+                        <figure>
+                            <img src={Img} alt="" />
+                        </figure>
+                    </li>
+                    <li>
+                        <figure>
+                            <img src={Img} alt="" />
+                        </figure>
+                    </li>
+                    </ul>
+                </aside>
+                <article className="product-details">
+                    <header>
+                        <strong>Titulo</strong>
+                        <del>$1.000.000</del>
+                        <em>$ 3.000.000</em>
+                        <div className="caracteristicas">
+                            <ul>
+                                <li>
+                                    Categoria: Remeras
+                                </li>
+                                <li>
+                                    Subcategoria: 100% Algodon, 100% Lino, Mangas cortas
+                                </li>
+                            </ul>
+                        </div>
+                    </header>
                     <ul>
                         <li>
-                            Categoria: Remeras
+                            <div className='input'>
+                                <button>
+                                    <Minus />
+                                </button>
+                                <input type="number" />
+                                <button>
+                                    <Plus />   
+                                </button>
+                            </div>
                         </li>
                         <li>
-                            Subcategoria: 100% Algodon, 100% Lino, Mangas cortas
+                            <div className='btn-carrito'>
+                                <button>
+                                    Añadir al carrito
+                                </button>
+                            </div>
+                        </li>
+                        <li>
+                            <button className='btn-share'>
+                                <Share />
+                            </button>
                         </li>
                     </ul>
-                </div>
-                <ul>
-                    <li>
-                        <div>
-                            <button>
-
-                            </button>
-                            <input type="number" />
-                            <button>
-                                
-                            </button>
-                        </div>
-                    </li>
-                    <li></li>
-                    <li></li>
-                </ul>
-            </article>
+                </article>
+            </section>
             <div className="product-description">
-
+                <p>
+                    Lorem ipsum dolor sit amet consectetur adipisicing elit. 
+                    Assumenda, facere dolorum eligendi voluptatem ex similique placeat qui. 
+                    Amet ipsam quas deserunt, nam aperiam excepturi, omnis dolores aliquid reprehenderit, pariatur rerum!
+                </p>
             </div>
-        </section>
+        </>
     )
 }
