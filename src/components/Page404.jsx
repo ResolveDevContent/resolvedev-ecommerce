a48b382acced76e1bf50df219495e490ad58ffce
LucasMezza <mezza.lucas12@gmail.com>
2024-05-17 12:13:59 -0300

    login component

--- src/components/Page404.jsx
+++ src/components/Page404.jsx
@@ -1,11 +1,11 @@
-// import Resolve from "../images/resolvedevverde.png"
+import Resolve from "../images/resolvedevverde.png"
 
 export const Page404 = () => {
     return (
         <section className="d-flex flex-column align-items-center mt-5">
             <h1 className="fw-bolder">Error 404</h1>
             <p>Esta página no existe</p>
-            <img src="" alt=""/>
+            <img src={Resolve} alt=""/>
         </section>
     )
 }
