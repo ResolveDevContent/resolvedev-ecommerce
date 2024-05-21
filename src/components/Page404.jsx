6dbee7cd4994852ace614e54c239f152c11226ba
LucasMezza <mezza.lucas12@gmail.com>
2024-05-21 11:00:42 -0300

    notfound page

--- src/components/Page404.jsx
+++ src/components/Page404.jsx
@@ -2,8 +2,8 @@ import Resolve from "../images/resolvedevverde.png"
 
 export const Page404 = () => {
     return (
-        <section className="d-flex flex-column align-items-center mt-5">
-            <h1 className="fw-bolder">Error 404</h1>
+        <section id="notFound">
+            <h1>Error 404</h1>
             <p>Esta página no existe</p>
             <img src={Resolve} alt=""/>
         </section>
