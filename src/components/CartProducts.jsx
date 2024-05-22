25fe3481062cda40ac922bc244e2f664503e5a4b
juaniB12 <bjuan.ignacio11@gmail.com>
2024-05-22 16:30:01 -0300

    changes

--- src/components/CartProducts.jsx
+++ src/components/CartProducts.jsx
@@ -9,7 +9,7 @@ export const CartProducts = ({products}) => {
         return Object.values(data).slice(1,4)
     })
 
-    console.log(columns, rows)
+    console.log(products, columns, rows)
 
     return (
         <div className="table">
@@ -23,32 +23,34 @@ export const CartProducts = ({products}) => {
                 <li>Cantidad</li>
                 <li></li>
             </ul>
-            <ul className='row'>
-                <li className='product-img'>
-                    <img src={Prod} alt='product-img' />
-                </li>
-                {rows[0].map((product, index) => (
-                    <li key={index}>
-                        <span>{product}</span>
-                    </li> 
-                ))}
-                <li>
-                    <div className='input'>
-                        <button>
-                            <Minus />
-                        </button>
-                        <input type="number" defaultValue={1}/>
-                        <button>
-                            <Plus />   
-                        </button>
-                    </div>
-                </li>
-                <li>
-                    <a href="#">
-                        <Trash />
-                    </a>
-                </li>
-            </ul>
+            {rows.map((row, index) => (
+                <ul className='row'>
+                    <li className='product-img'>
+                        <img src={Prod} alt='product-img' />
+                    </li>
+                    {row.map((product, idx) => (
+                        <li key={idx}>
+                            <span>{product}</span>
+                        </li> 
+                    ))}
+                    <li>
+                        <div className='input'>
+                            <button>
+                                <Minus />
+                            </button>
+                            <input type="number" defaultValue={1}/>
+                            <button>
+                                <Plus />   
+                            </button>
+                        </div>
+                    </li>
+                    <li>
+                        <a href="#">
+                            <Trash />
+                        </a>
+                    </li>
+                </ul>
+            ))}
         </div>
     )
 }
