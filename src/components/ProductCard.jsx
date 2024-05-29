1be869558d190cdb695a13c7c5d832df83ef47c1
LucasMezza <mezza.lucas12@gmail.com>
2024-05-29 11:31:40 -0300

    prod arrows

--- src/components/ProductCard.jsx
+++ src/components/ProductCard.jsx
@@ -1,17 +1,21 @@
 import { Link } from 'react-router-dom'
 import { useCart } from 'resolvedev-cart'
+import { Arrows } from './Arrows'
+import { useRef } from 'react'
 
 export const ProductCard = ({item}) => {
     const { addToCart, removeFromCart, cart } = useCart()
     const { _id, imagenes, nombre, caracteristicas, fmt_modified_price, fmt_precio, descuento, carrito } = item
     const inCart = cart.some(prod => prod._id == item._id)
+    const scrollable = useRef();
 
     return (
         <li className="producto" data-id={_id}>
             <article data-discount={descuento}>
                 <Link to={`/producto/${_id}`} >
+                    <Arrows scrollable={scrollable}/>
                     <span className='loader'></span>
-                    <ul className='imagenes'>
+                    <ul ref={scrollable} className='imagenes'>
                         {imagenes.map((img, idx) => (
                             <li key={idx}>
                                 <img src={img} alt="" />
