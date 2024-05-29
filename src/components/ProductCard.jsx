abf3cd29929bc108509651d7daa11afbe91e8e3f
juaniB12 <bjuan.ignacio11@gmail.com>
2024-05-29 16:56:30 -0300

    change product cart

--- src/components/ProductCard.jsx
+++ src/components/ProductCard.jsx
@@ -8,7 +8,7 @@ export const ProductCard = ({item}) => {
     const { _id, imagenes, nombre, caracteristicas, fmt_modified_price, fmt_precio, descuento, carrito } = item
     const inCart = cart.some(prod => prod._id == item._id)
     const scrollable = useRef();
-
+    console.log(item)
     return (
         <li className="producto" data-id={_id}>
             <article data-discount={descuento}>
@@ -26,7 +26,7 @@ export const ProductCard = ({item}) => {
             </article>
             <footer>
                 <strong>{nombre}</strong>
-                <span>{caracteristicas[0]}</span>
+                <span>{caracteristicas[0].nombre}</span>
                 <div className='price'>
                     <del>{fmt_modified_price}</del>
                     <em>${fmt_precio}</em>
