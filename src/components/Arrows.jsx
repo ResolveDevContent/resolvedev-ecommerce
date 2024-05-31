29e9dffa670fb64b7f16e70e9171b703f8fdcf6c
juaniB12 <bjuan.ignacio11@gmail.com>
2024-05-31 11:29:37 -0300

    changes

--- src/components/Arrows.jsx
+++ src/components/Arrows.jsx
@@ -1,14 +1,28 @@
+import { useEffect, useRef } from 'react'
 import { ArrowLeft, ArrowRight } from '../icons/Icons'
 
 export const Arrows = ({scrollable}) => {
+    const arrows = useRef()
     
     const handleClick = (e, num) => {
         e.preventDefault()
+
         scrollable.current.scrollLeft += scrollable.current.clientWidth * Number(num)
     }
 
+    useEffect(() => {
+        if(scrollable.current.querySelectorAll(".producto").length > 0) {
+            const ul = Array.from(scrollable.current.querySelectorAll(".producto"))
+            
+            if(ul.length <= 3) {
+                arrows.current.style.display = "none"
+            }
+        }
+
+    }, [scrollable.current])
+
     return (
-        <nav className='carousel-arrow'>
+        <nav className='carousel-arrow' ref={arrows}>
             <button onClick={(e) => handleClick(e, -1)}>
                 <ArrowLeft/>
             </button>
